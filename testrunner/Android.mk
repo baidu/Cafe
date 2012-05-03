618aeadc420f67ca8198947aac0b04c269238391
luxiaoyu01 <luxiaoyu01@baidu.com>
2012-05-03 17:01:05 +0800

    remove YiOperator

--- testrunner/Android.mk
+++ testrunner/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := testcafe
+LOCAL_MODULE := cafe
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_SRC_FILES += \
