67cfa6a339609b49f05de3cf22d5cbcad4c20332
luxiaoyu <luxiaoyu01@baidu.com>
2012-06-20 15:22:32 +0800

    add cafe_setup.bat

---
+++ cafe_setup.bat
@@ -0,0 +1,5 @@
+adb shell service call window 1 i32 4939
+adb shell ps | findstr "monkey" > a.inf
+for /f "eol=; tokens=2 delims=, " %%i in (a.inf) do @adb shell kill %%i
+del a.inf
+adb shell monkey --port 4938 --ignore-crashes --ignore-security-exceptions --ignore-native-crashes -v -v
