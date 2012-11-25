da533297504f8caa6b6464a575d58b3ab3a63163
luxiaoyu01 <luxiaoyu01@baidu.com>
2012-11-26 07:44:27 +0800

    replace junitreport with com.zutubi.android.junitreport.JUnitReportTestRunner

--- testrunner/Android.mk
+++ testrunner/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_SRC_FILES += \
         src/com/baidu/cafe/remote/IRemoteArms.aidl
 
-LOCAL_STATIC_JAVA_LIBRARIES := lib3party
+LOCAL_STATIC_JAVA_LIBRARIES := librobotium libzutubi
 
 LOCAL_JAVA_LIBRARIES := android.test.runner
 
@@ -17,6 +17,7 @@ include $(BUILD_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := lib3party:third/robotium-solo-3.1.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := librobotium:third/robotium-solo-3.1.jar \
+	libzutubi:third/android-junit-report-1.5.8.jar
 
 include $(BUILD_MULTI_PREBUILT) 
