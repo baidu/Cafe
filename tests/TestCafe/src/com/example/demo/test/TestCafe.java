fbf0994d9103ca9dff5e1c4378bce0a14d4a2777
luxiaoyu01 <luxiaoyu01@baidu.com>
2012-12-24 21:57:21 +0800

    update ViewRecorder

--- tests/TestCafe/src/com/example/demo/test/TestCafe.java
+++ tests/TestCafe/src/com/example/demo/test/TestCafe.java
@@ -46,7 +46,6 @@ public class TestCafe extends CafeTestCase {
      * NOTICE: 运行case前需要开启cafe_setup.bat
      */
     public void test_sample() {
-        
         for (View view : local.getCurrentViews()) {
             try {
                 System.out.println(getInstrumentation().getTargetContext().getResources().getString(view.getId()));
--- tests/TestRecord/src/com/example/demo/test/TestCafe.java
+++ tests/TestRecord/src/com/example/demo/test/TestCafe.java
@@ -42,77 +42,48 @@ public class TestCafe extends CafeTestCase {
         super.tearDown();
     }
 
-    /**
-     * NOTICE: 运行case前需要开启cafe_setup.bat
-     */
-    public void test_sample() {
-        local.sleep(2000);
-            for (View view : local.getCurrentViews()) {
-                try {
-                    System.out.println("!!!!!!!!!!"+view.getId());
-                    System.out.println(getInstrumentation().getTargetContext().getResources().getString(view.getId()));
-                } catch (Exception e) {
+    private int getRStringId(String packageName, String stringName) {
+        try {
+            Class[] classes = Class.forName(packageName + ".R").getDeclaredClasses();
+            int stringIndex = 0;
+            for (int i = 0; i < classes.length; i++) {
+                System.out.println("class: "+classes[i].getName());
+                if (classes[i].getName().indexOf("$string") != -1) {
+                    stringIndex = i;
                 }
             }
-        local.beginRecordCode();
-        local.sleep(2000000);
+            return (Integer) classes[stringIndex].getDeclaredField(stringName).get(
+                    classes[stringIndex].newInstance());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
-    private void history() {
-        remote.pressKey(KeyEvent.KEYCODE_HOME);
-        //点击锁屏键
-        remote.pressKey(KeyEvent.KEYCODE_POWER);
-        //点击音量键
-        remote.pressKey(KeyEvent.KEYCODE_VOLUME_DOWN);
-        //点击菜单键
-        remote.pressKey(KeyEvent.KEYCODE_MENU);
-        //点击返回键
-        remote.pressKey(KeyEvent.KEYCODE_BACK);
-        //点击搜索键
-        remote.pressKey(KeyEvent.KEYCODE_SEARCH);
-        //卸载sd卡
-        remote.unmount();
-        //安装sd卡
-        remote.mount();
-        //杀后台进程
-        remote.killBackgroundProcesses("com.android.launcher");
-        //启动其他应用
-        remote.launchActivity("com.android.launcher");
-        //输入字符
-        local.enterText(0, "Cafe");//0表示第一个搜索框
-        //长按
-        //        local.clickLongOnView(fakeView);
-        local.clickLongInList(1);
-        local.clickLongOnText("Cafe");
-        local.clickLongOnScreen(100, 100);
-        //点击文字（任何控件上的文字）
-        local.clickOnText("Cafe");
-        //获取控件
-        local.getCurrentViews(Button.class);
-        //获取logcat
-        String[] logs = remote.getLog();
-        //获取电量
-        remote.getBatteryLevel();
-        //获取亮度
-        remote.getScreenBrightness();
-        //从(0,0)拖动到(100,100)
-        local.drag(0, 100, 0, 100, 10);
-        //放大缩小
-        /**
-         * zoom screen
-         * 
-         * @param start
-         *            the start position e.g. new int[]{0,0,1,2}; means two
-         *            pointers start at {0,0} and {1,2}
-         * @param end
-         *            the end position e.g. new int[]{100,110,200,220}; means
-         *            two pointers end at {100,110} and {200,220}
-         */
-        local.zoom(new int[] { 0, 0, 1, 2 }, new int[] { 100, 110, 200, 220 });
-        //跨进程点击
-        remote.clickViewByText("通讯录");
-        if (remote.waitForActivity("com.android.contacts.DialtactsContactsEntryActivity", 5000)) {
-            Log.i("at com.android.contacts.DialtactsContactsEntryActivity");
+    public void test_sample() {
+        getRStringId("com.baidu.news","");
+        local.sleep(2000);
+        /*
+        for (View view : local.getCurrentViews()) {
+            try {
+                System.out.println("!!!!!!!!!!"+view.getId());
+                System.out.println(getInstrumentation().getTargetContext().getResources().getString(view.getId()));
+            } catch (Exception e) {
+            }
         }
+        local.beginRecordCode();
+        local.sleep(2000000);
+        */
     }
+
 }
