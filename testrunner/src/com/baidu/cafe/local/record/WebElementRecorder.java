ed0c972bdb8f8d07a448ffa27da736250569d98b
luxiaoyu01 <luxiaoyu01@baidu.com>
2013-04-27 14:33:38 +0800

    merge from webrecorder

--- testrunner/src/com/baidu/cafe/local/record/WebElementRecorder.java
+++ testrunner/src/com/baidu/cafe/local/record/WebElementRecorder.java
@@ -5,17 +5,24 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.json.JSONObject;
 
 import android.os.SystemClock;
 import android.view.View;
 import android.webkit.JsPromptResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.baidu.cafe.CafeTestCase;
 import com.baidu.cafe.local.DESEncryption;
 import com.baidu.cafe.local.LocalLib;
 import com.baidu.cafe.local.Log;
+import com.baidu.cafe.local.record.ViewRecorder.RecordMotionEvent;
 
 /**
  * one object one webview
@@ -26,233 +33,382 @@ import com.baidu.cafe.local.Log;
  * @todo
  */
 public class WebElementRecorder {
-    private ViewRecorder           viewRecorder           = null;
-    private WebView                webView                = null;
-    private WebElementRecordClient webElementRecordClient = null;
-    private WebElementEventCreator webElementEventCreator = null;
+	private ViewRecorder viewRecorder = null;
+	private WebView webView = null;
+	private WebElementRecordClient webElementRecordClient = null;
+	private WebElementEventCreator webElementEventCreator = null;
+	private final static int TIMEOUT_NEXT_EVENT = 500;
 
-    public WebElementRecorder(ViewRecorder viewRecorder) {
-        this.viewRecorder = viewRecorder;
-        this.webElementEventCreator = new WebElementEventCreator(viewRecorder);
-        this.webElementRecordClient = new WebElementRecordClient(viewRecorder.getLocalLib(),
-                webElementEventCreator);
-    }
+	/**
+	 * lock for OutputEventQueue
+	 */
+	private static String mSyncWebElementRecordEventQueue = new String("mSyncWebElementRecordEventQueue");
 
-    /**
-     * set hook listener on WebView body
-     * 
-     * @param webView
-     * @return
-     */
-    public void handleWebView(final WebView webView) {
-        if (webView == null) {
-            return;
-        }
-        print("start monitor WebView: " + webView);
-        this.webView = webView;
+	private Queue<WebElementRecordEvent> mWebElementRecordEventQueue = new LinkedList<WebElementRecordEvent>();
 
-        // monitor WebView
-        new Thread(new Runnable() {
+	public WebElementRecorder(ViewRecorder viewRecorder) {
+		this.viewRecorder = viewRecorder;
+		this.webElementEventCreator = new WebElementEventCreator(viewRecorder);
+		this.webElementRecordClient = new WebElementRecordClient(viewRecorder.getLocalLib(), webElementEventCreator);
+		this.mWebElementRecordEventQueue.clear();
+	}
 
-            @Override
-            public void run() {
-                while (true) {
-                    hookWebView(webView);
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-    }
+	public boolean offerWebElementRecordEventQueue(WebElementRecordEvent event) {
+		synchronized (mSyncWebElementRecordEventQueue) {
+			return mWebElementRecordEventQueue.offer(event);
+		}
+	}
 
-    private void hookWebView(final WebView webView) {
-        webElementEventCreator.prepareForStart();
-        webElementRecordClient.setWebElementRecordClient(webView);
-        final String javaScript = getJavaScriptAsString();
-        viewRecorder.getLocalLib().getCurrentActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (webView != null) {
-                    webView.loadUrl("javascript:" + javaScript);
-                }
-            }
-        });
-    }
+	public WebElementRecordEvent peekWebElementRecordEventQueue() {
+		synchronized (mSyncWebElementRecordEventQueue) {
+			return mWebElementRecordEventQueue.peek();
+		}
+	}
 
-    private String getJavaScriptAsString() {
-        StringBuffer javaScript = new StringBuffer();
-        try {
-            //            InputStream fis = getClass().getResourceAsStream("WebElementRecorder.js");
-            InputStream fis = new FileInputStream(CafeTestCase.mTargetFilesDir
-                    + "/WebElementRecorder.js");
-            BufferedReader input = new BufferedReader(new InputStreamReader(fis));
-            String line = null;
-            while ((line = input.readLine()) != null) {
-                javaScript.append(line);
-                javaScript.append("\n");
-            }
-            input.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return javaScript.toString();
-    }
+	public WebElementRecordEvent pollWebElementRecordEventQueue() {
+		synchronized (mSyncWebElementRecordEventQueue) {
+			return mWebElementRecordEventQueue.poll();
 
-    class WebElementRecordClient extends WebChromeClient {
-        LocalLib                local;
-        WebElementEventCreator  webElementEventCreator;
-        private WebChromeClient webElementRecordClient;
+		}
+	}
 
-        /**
-         * Constructs this object.
-         * 
-         * @param instrumentation
-         *            the {@code Instrumentation} instance
-         * @param webElementCreator
-         *            the {@code WebElementCreator} instance
-         */
+	private void sleep(long time) {
+		try {
+			Thread.sleep(time);
+		} catch (Exception ignored) {
+		}
+	}
 
-        public WebElementRecordClient(LocalLib local, WebElementEventCreator webElementEventCreator) {
-            this.local = local;
-            this.webElementEventCreator = webElementEventCreator;
-            webElementRecordClient = this;
-        }
+	/**
+	 * set hook listener on WebView body
+	 * 
+	 * @param webView
+	 * @return
+	 */
+	public void handleWebView(final WebView webView) {
+		if (webView == null) {
+			return;
+		}
+		print("start monitor WebView: " + webView);
+		this.webView = webView;
+		// monitor WebView
+		hookWebView(webView);
+		handleWebElementRecordEventQueue();
+	}
 
-        /**
-         * Enables JavaScript in the given {@code WebViews} objects.
-         * 
-         * @param webViews
-         *            the {@code WebView} objects to enable JavaScript in
-         */
+	public void handleWebElementRecordEventQueue() {
+		new Thread(new Runnable() {
+			public void run() {
+				ArrayList<WebElementRecordEvent> events = new ArrayList<WebElementRecordEvent>();
+				while (true) {
+					WebElementRecordEvent lastRecordEvent = null;
+					long endTime = System.currentTimeMillis() + TIMEOUT_NEXT_EVENT;
+					WebElementRecordEvent e = null;
+					while (true) {
+						e = null;
+						if ((e = pollWebElementRecordEventQueue()) != null) {
+							// here comes a record event
+							endTime = System.currentTimeMillis() + TIMEOUT_NEXT_EVENT;
+							if (lastRecordEvent == null) {
+								// if e is the first record event
+								events.add(e);
+								lastRecordEvent = e;
+							} else {
+								if (e.time > lastRecordEvent.time
+										&& e.familyString.equals(lastRecordEvent.familyString)) {
+									events.add(e);
+									lastRecordEvent = e;
+								} else {
+									offerOutputEventQueue(events);
+									lastRecordEvent = null;
+									events.clear();
+								}
+							}
+						} else {
+							// wait until timeout, then offerOutputEventQueue
+							if (System.currentTimeMillis() > endTime) {
+								offerOutputEventQueue(events);
+								lastRecordEvent = null;
+								events.clear();
+								sleep(50);
+								break;
+							}
+						}
+						sleep(10);
+					}
+				}
+			}
+		}).start();
+	}
 
-        public void setWebElementRecordClient(final WebView webView) {
-            if (webView != null) {
-                local.runOnMainSync(new Runnable() {
-                    @Override
-                    public void run() {
-                        webView.getSettings().setJavaScriptEnabled(true);
-                        webView.setWebChromeClient(webElementRecordClient);
-                    }
-                });
-            }
-        }
+	private boolean offerOutputEventQueue(ArrayList<WebElementRecordEvent> events) {
+		if (events.isEmpty()) {
+			return false;
+		}
+		boolean eventOffered = false;
+		boolean isTouchstart = false;
+		boolean isTouchmove = false;
+		for (WebElementRecordEvent e : events) {
+			if ("touchstart".equals(e.action)) {
+				isTouchstart = true;
+			} else if ("touchmove".equals(e.action)) {
+				if (isTouchstart) {
+					isTouchmove = true;
+				} else {// touchmove without touchstart before, note as illegal
+					eventOffered = false;
+					break;
+				}
+			} else if ("touchcancel".equals(e.action)) {
+				eventOffered = false;
+				break;
+			} else if ("touchend".equals(e.action)) {
+				// when it comes touchstart -> touchend without touchmove in the
+				// list, offer a click action
+				if (isTouchstart && !isTouchmove) {
+					eventOffered = true;
+					// break;
+				} else {
+					// touchstart -> touchmove -> touchend
+					eventOffered = false;
+					// break;
+				}
+				break;
+			} else if ("click".equals(e.action)) {
+				eventOffered = true;
+				break;
+			}
+		}
+		if (eventOffered) {
+			WebElementRecordEvent e = events.get(0);
+			OutputEvent outputEvent = new WebElementClickEvent(e.view);
+			outputEvent.setCode(String.format("local.clickOnWebElementByFamilyString(\"%s\"); // Click On [%s]",
+					e.familyString, e.tag));
+			viewRecorder.offerOutputEventQueue(outputEvent);
+		}
+		return eventOffered;
+	}
 
-        /**
-         * Overrides onJsPrompt in order to create {@code WebElement} objects
-         * based on the web elements attributes prompted by the injections of
-         * JavaScript
-         */
+	public void hookWebView(final WebView webView) {
+		webElementEventCreator.prepareForStart();
+		webElementRecordClient.setWebElementRecordClient(webView);
+		final String javaScript = getJavaScriptAsString();
+		hookWebElements(webView, javaScript);
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				hookWebElements(view, javaScript);
+			}
+		});
+	}
 
-        @Override
-        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue,
-                JsPromptResult r) {
+	/**
+	 * @param webView
+	 * @param javaScript
+	 */
+	public void hookWebElements(final WebView webView, final String javaScript) {
+		viewRecorder.getLocalLib().getCurrentActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				if (webView != null) {
+					webView.loadUrl("javascript:" + javaScript);
+				}
+			}
+		});
+	}
 
-            if (message != null) {
-                if (message.equals("WebElementRecorder-finished")) {
-                    webElementEventCreator.setFinished(true);
-                } else {
-                    webElementEventCreator.createWebElementEvent(message, view);
-                }
-            }
-            r.confirm();
-            return true;
-        }
+	private String getJavaScriptAsString() {
+		StringBuffer javaScript = new StringBuffer();
+		try {
+			// InputStream fis =
+			// getClass().getResourceAsStream("WebElementRecorder.js");
+			InputStream fis = new FileInputStream(CafeTestCase.mTargetFilesDir + "/WebElementRecorder.js");
+			BufferedReader input = new BufferedReader(new InputStreamReader(fis));
+			String line = null;
+			while ((line = input.readLine()) != null) {
+				javaScript.append(line);
+				javaScript.append("\n");
+			}
+			input.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return javaScript.toString();
+	}
 
-    }
+	class WebElementRecordClient extends WebChromeClient {
+		LocalLib local;
+		WebElementEventCreator webElementEventCreator;
+		private WebChromeClient webElementRecordClient;
 
-    private void print(String message) {
-        if (ViewRecorder.DEBUG) {
-            Log.i("ViewRecorder", message);
-        } else {
-            Log.i("ViewRecorder", DESEncryption.encryptStr(message));
-        }
-    }
+		/**
+		 * Constructs this object.
+		 * 
+		 * @param instrumentation
+		 *            the {@code Instrumentation} instance
+		 * @param webElementCreator
+		 *            the {@code WebElementCreator} instance
+		 */
 
-    class WebElementEventCreator {
-        private boolean             isFinished   = false;
-        private ViewRecorder        viewRecorder = null;
+		public WebElementRecordClient(LocalLib local, WebElementEventCreator webElementEventCreator) {
+			this.local = local;
+			this.webElementEventCreator = webElementEventCreator;
+			webElementRecordClient = this;
+		}
 
-        public final static boolean DEBUG        = true;
+		/**
+		 * Enables JavaScript in the given {@code WebViews} objects.
+		 * 
+		 * @param webViews
+		 *            the {@code WebView} objects to enable JavaScript in
+		 */
 
-        public WebElementEventCreator(ViewRecorder viewRecorder) {
-            this.viewRecorder = viewRecorder;
-        }
+		public void setWebElementRecordClient(final WebView webView) {
+			if (webView != null) {
+				local.runOnMainSync(new Runnable() {
+					public void run() {
+						webView.getSettings().setJavaScriptEnabled(true);
+						webView.setWebChromeClient(webElementRecordClient);
+					}
+				});
+			}
+		}
 
-        public void prepareForStart() {
-            setFinished(false);
-        }
+		/**
+		 * Overrides onJsPrompt in order to create {@code WebElement} objects
+		 * based on the web elements attributes prompted by the injections of
+		 * JavaScript
+		 */
 
-        public void createWebElementEvent(String information, WebView webView) {
-            String[] data = information.split(";,");
-            String type = data[0];
-            String code = data[1];
-            WebElementEvent event = null;
+		@Override
+		public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult r) {
 
-            if (DEBUG) {
-            	System.out.println("createWebElementEvent, information : " + information);
-                code = "local.dumpPage();\n" + code;
-            }
+			if (message != null) {
+				if (message.equals("WebElementRecorder-finished")) {
+					webElementEventCreator.setFinished(true);
+				} else {
+					webElementEventCreator.createWebElementEvent(message, view);
+				}
+			}
+			r.confirm();
+			return true;
+		}
+	}
 
-            if ("click".equals(type)) {
-                event = new WebElementClickEvent(webView);
-                event.setCode(code);
-            } else if ("change".equals(type)) {
-                event = new WebElementChangeEvent(webView);
-                event.setCode(code);
-            } else {
-                event = null;
-            }
+	private void print(String message) {
+		if (ViewRecorder.DEBUG) {
+			Log.i("ViewRecorder", message);
+		} else {
+			Log.i("ViewRecorder", DESEncryption.encryptStr(message));
+		}
+	}
 
-            if (event != null) {
-                print("!!!!!!!!!!!!WebElementEventCreator:" + event);
-                viewRecorder.offerOutputEventQueue(event);
-            }
-        }
+	class WebElementEventCreator {
+		private boolean isFinished = false;
+		private ViewRecorder viewRecorder = null;
 
-        public void setFinished(boolean isFinished) {
-            this.isFinished = isFinished;
-        }
+		public final static boolean DEBUG = true;
 
-        public boolean isFinished() {
-            return isFinished;
-        }
+		public WebElementEventCreator(ViewRecorder viewRecorder) {
+			this.viewRecorder = viewRecorder;
+		}
 
-        private boolean waitForWebElementsToBeCreated() {
-            final long endTime = SystemClock.uptimeMillis() + 20;
+		public void prepareForStart() {
+			setFinished(false);
+		}
 
-            while (SystemClock.uptimeMillis() < endTime) {
+		public void createWebElementEvent(String information, WebView webView) {
+			String action = "";
+			long time = 0;
+			String familyString = "";
+			int x = 0;
+			int y = 0;
+			int width = 0;
+			int height = 0;
+			String value = "";
+			String tag = "";
+			try {
+				JSONObject jsonObject = new JSONObject(information);
+				action = jsonObject.getString("action");
+				time = Long.parseLong(jsonObject.getString("time"));
+				familyString = jsonObject.getString("familyString");
+				x = Integer.parseInt(jsonObject.getString("left"));
+				y = Integer.parseInt(jsonObject.getString("top"));
+				width = Integer.parseInt(jsonObject.getString("width"));
+				height = Integer.parseInt(jsonObject.getString("height"));
+				value = jsonObject.isNull("value") ? "" : jsonObject.getString("value");
+				tag = jsonObject.isNull("tag") ? "" : jsonObject.getString("tag");
+			} catch (Exception ignored) {
+				if (DEBUG) {
+					ignored.printStackTrace();
+				}
+			}
+			float scale = webView.getScale();
+			int[] locationOfWebViewXY = new int[2];
+			webView.getLocationOnScreen(locationOfWebViewXY);
 
-                if (isFinished) {
-                    return true;
-                }
-                try {
-                    Thread.sleep(2);
-                } catch (InterruptedException ignored) {
-                }
-            }
-            return false;
-        }
-    }
+			int locationX = (int) (locationOfWebViewXY[0] + (x + (Math.floor(width / 2))) * scale);
+			int locationY = (int) (locationOfWebViewXY[1] + (y + (Math.floor(height / 2))) * scale);
+			System.out.println("[action:" + action + "] [familyString:" + familyString + "] [locationX:" + locationX
+					+ "] [locationY:" + locationY + "] [time:" + time + "] [tag:" + tag + "]");
+			WebElementRecordEvent event = new WebElementRecordEvent(webView, familyString, action, locationX,
+					locationY, time, value, tag);
+			offerWebElementRecordEventQueue(event);
+		}
 
-    class WebElementEvent extends OutputEvent {
+		public void setFinished(boolean isFinished) {
+			this.isFinished = isFinished;
+		}
 
-    }
+		public boolean isFinished() {
+			return isFinished;
+		}
 
-    class WebElementClickEvent extends WebElementEvent {
-        public WebElementClickEvent(View view) {
-            this.view = view;
-            this.priority = PRIORITY_WEBELEMENT_CLICK;
-        }
-    }
+		private boolean waitForWebElementsToBeCreated() {
+			final long endTime = SystemClock.uptimeMillis() + 20;
+			while (SystemClock.uptimeMillis() < endTime) {
+				if (isFinished) {
+					return true;
+				}
+				sleep(2);
+			}
+			return false;
+		}
+	}
 
-    class WebElementChangeEvent extends WebElementEvent {
-        public WebElementChangeEvent(View view) {
-            this.view = view;
-            this.priority = PRIORITY_WEBELEMENT_CHANGE;
-        }
-    }
+	class WebElementRecordEvent {
+		public View view;
+		public String familyString;
+		public String action;
+		public int x;
+		public int y;
+		public long time;
+		public String value;
+		public String tag;
+
+		public WebElementRecordEvent(View view, String familyString, String action, int x, int y, long time,
+				String value, String tag) {
+			this.view = view;
+			this.familyString = familyString;
+			this.action = action;
+			this.x = x;
+			this.y = y;
+			this.time = time;
+			this.value = value;
+			this.tag = tag;
+		}
+	}
+
+	class WebElementClickEvent extends OutputEvent {
+		public WebElementClickEvent(View view) {
+			this.view = view;
+			this.priority = PRIORITY_WEBELEMENT_CLICK;
+		}
+	}
+
+	class WebElementChangeEvent extends OutputEvent {
+		public WebElementChangeEvent(View view) {
+			this.view = view;
+			this.priority = PRIORITY_WEBELEMENT_CHANGE;
+		}
+	}
 
 }
--- testservice/assets/WebElementRecorder.js
+++ testservice/assets/WebElementRecorder.js
@@ -1,37 +1,66 @@
-if(typeof _cafe === "undefined") {
-	function getElementFamilyString(element) {
-	    var familyString = '';
-	    for ( ; element && element.nodeType == 1; element = element.parentNode ) {
-	        var children = element.parentNode.childNodes;
-	        var id = '';
-	        var i = 0;
-	        for(; i < children.length; i++) {
-	            if (children[i] === element) {
-	                break;
-	            }
-	        }
-	        familyString = '-' + i + familyString;
-	    }
-	    return familyString.substring(1);
-	}
-	function onClickCallback() {
-		var event = arguments[0];
-		prompt(event.type + ";,local.clickOnWebElementByFamilyString(\"" + getElementFamilyString(event.target) + "\");");
-		finished();
-	}
-	function onChangeCallback() {
-		var event = arguments[0];
-		prompt(event.type + ";,local.enterTextInWebElementByFamilyString(\"" + getElementFamilyString(event.target) + "\"), \"" + event.target.value + "\");");
-		finished();
-	}
-	function finished() {
-		prompt('WebElementRecorder-finished');
-	}
-	
-	document.removeEventListener('click', onClickCallback);
-	document.removeEventListener('change', onChangeCallback);
-	
-	document.addEventListener('click', onClickCallback, false);
-	document.addEventListener('change', onChangeCallback, false);
-	var _cafe = new Object();
+var Cafe = function() {};
+Cafe.prototype.getElementFamilyString = function(element) {
+    var familyString = '';
+    for ( ; element && element.nodeType == 1; element = element.parentNode ) {
+        var children = element.parentNode.childNodes;
+        var id = '';
+        var i = 0;
+        for(; i < children.length; i++) {
+            if (children[i] === element) {
+                break;
+            }
+        }
+        familyString = '-' + i + familyString;
+    }
+    return familyString.substring(1);
 }
+Cafe.prototype.onEventCallback = function() {
+	var event = arguments[0];
+	if(this._familyString === event.target._familyString) {
+		var rect = this.getBoundingClientRect();
+		prompt("{'action':'" + event.type + "','time':'" + (new Date -0) + "','familyString':'" + event.target._familyString + "','left':'" + rect.left + "','top':'" + rect.top + "','width':'" + rect.width + "','height':'" + rect.height + "','tag':'" +this.tagName + "'}");
+		Cafe.prototype.finished();
+	}
+}
+Cafe.prototype.onTouchmoveCallback = function() {
+	var event = arguments[0];
+	if(this._familyString === event.target._familyString) {
+		var rect = this.getBoundingClientRect();
+		prompt("{'action':'" + event.type + "','time':'" + (new Date -0) + "','familyString':'" + event.target._familyString + "','left':'" + rect.left + "','top':'" + rect.top + "','width':'" + rect.width + "','height':'" + rect.height + "','tag':'" +this.tagName + "'}");
+		Cafe.prototype.finished();
+	}
+}
+Cafe.prototype.onChangeCallback = function() {
+	var event = arguments[0];
+	if(this._familyString === event.target._familyString) {
+		var rect = this.getBoundingClientRect();
+		prompt("{'action':'" + event.type + "','time':'" + (new Date -0) + "','familyString':'" + event.target._familyString + "','left':'" + rect.left + "','top':'" + rect.top + "','width':'" + rect.width + "','height':'" + rect.height + "','value':'" + event.target.value + "','tag':'" +this.tagName + "'}");
+		Cafe.prototype.finished();
+	}
+}
+Cafe.prototype.finished = function() {
+	prompt('WebElementRecorder-finished');
+}
+Cafe.prototype.hookWebDocument = function() {
+	var walk=document.createTreeWalker(document.body,NodeFilter.SHOW_ALL,null,false); 
+	while(n=walk.nextNode()){
+		if(typeof n._cafe === "undefined") {
+			n.removeEventListener('touchstart', this.onEventCallback);
+			n.removeEventListener('touchend', this.onEventCallback);
+			n.removeEventListener('touchmove', this.onTouchmoveCallback);
+			n.removeEventListener('click', this.onEventCallback);
+			
+			n.addEventListener('touchstart', this.onEventCallback, false);
+			n.addEventListener('touchend', this.onEventCallback, false);
+			n.addEventListener('touchmove', this.onTouchmoveCallback, false);
+			n.addEventListener('click', this.onEventCallback, false);
+			n._familyString = this.getElementFamilyString(n);
+			n._cafe = new Object();
+		} else {
+			n._familyString = this.getElementFamilyString(n);
+		}
+	}
+}
+var _cafe = new Cafe();
+setInterval("_cafe.hookWebDocument()", 200);
+
