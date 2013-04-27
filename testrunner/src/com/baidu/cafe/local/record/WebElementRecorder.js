81d58cbdcb74355e44bb54306909e1c4e43c4286
leiming <leiming@leiming-HP-Compaq-6000-Pro-MT-PC.(none)>
2013-04-27 14:08:20 +0800

    save

--- testrunner/src/com/baidu/cafe/local/record/WebElementRecorder.js
+++ testrunner/src/com/baidu/cafe/local/record/WebElementRecorder.js
@@ -1,4 +1,5 @@
-function getElementFamilyString(element) {
+var Cafe = function() {};
+Cafe.prototype.getElementFamilyString = function(element) {
     var familyString = '';
     for ( ; element && element.nodeType == 1; element = element.parentNode ) {
         var children = element.parentNode.childNodes;
@@ -13,22 +14,52 @@ function getElementFamilyString(element) {
     }
     return familyString.substring(1);
 }
-function onClickCallback() {
+Cafe.prototype.onEventCallback = function() {
 	var event = arguments[0];
-	prompt(event.type + ";,local.clickOnWebElementByFamilyString(\"" + getElementFamilyString(event.target) + "\");");
-	finished();
+	if(this._familyString === event.target._familyString) {
+		var rect = this.getBoundingClientRect();
+		prompt("{'action':'" + event.type + "','time':'" + (new Date -0) + "','familyString':'" + event.target._familyString + "','left':'" + rect.left + "','top':'" + rect.top + "','width':'" + rect.width + "','height':'" + rect.height + "','tag':'" +this.tagName + "'}");
+		Cafe.prototype.finished();
+	}
 }
-function onChangeCallback() {
+Cafe.prototype.onTouchmoveCallback = function() {
 	var event = arguments[0];
-	prompt(event.type + ";,local.enterTextInWebElementByFamilyString(\"" + getElementFamilyString(event.target) + "\"), \"" + event.target.value + "\");");
-	finished();
+	if(this._familyString === event.target._familyString) {
+		var rect = this.getBoundingClientRect();
+		prompt("{'action':'" + event.type + "','time':'" + (new Date -0) + "','familyString':'" + event.target._familyString + "','left':'" + rect.left + "','top':'" + rect.top + "','width':'" + rect.width + "','height':'" + rect.height + "','tag':'" +this.tagName + "'}");
+		Cafe.prototype.finished();
+	}
 }
-function finished() {
+Cafe.prototype.onChangeCallback = function() {
+	var event = arguments[0];
+	if(this._familyString === event.target._familyString) {
+		var rect = this.getBoundingClientRect();
+		prompt("{'action':'" + event.type + "','time':'" + (new Date -0) + "','familyString':'" + event.target._familyString + "','left':'" + rect.left + "','top':'" + rect.top + "','width':'" + rect.width + "','height':'" + rect.height + "','value':'" + event.target.value + "','tag':'" +this.tagName + "'}");
+		Cafe.prototype.finished();
+	}
+}
+Cafe.prototype.finished = function() {
 	prompt('WebElementRecorder-finished');
 }
-
-document.removeEventListener('click', onClickCallback);
-document.removeEventListener('change', onChangeCallback);
-
-document.addEventListener('click', onClickCallback, false);
-document.addEventListener('change', onChangeCallback, false);
+Cafe.prototype.hookWebDocument = function() {
+	var walk=document.createTreeWalker(document.body,NodeFilter.SHOW_ALL,null,false); 
+	while(n=walk.nextNode()){
+		if(typeof n._cafe === "undefined") {
+			n.removeEventListener('touchstart', this.onEventCallback);
+			n.removeEventListener('touchend', this.onEventCallback);
+			n.removeEventListener('touchmove', this.onTouchmoveCallback);
+			n.removeEventListener('click', this.onEventCallback);
+			
+			n.addEventListener('touchstart', this.onEventCallback, false);
+			n.addEventListener('touchend', this.onEventCallback, false);
+			n.addEventListener('touchmove', this.onTouchmoveCallback, false);
+			n.addEventListener('click', this.onEventCallback, false);
+			n._familyString = this.getElementFamilyString(n);
+			n._cafe = new Object();
+		} else {
+			n._familyString = this.getElementFamilyString(n);
+		}
+	}
+}
+var _cafe = new Cafe();
+setInterval("_cafe.hookWebDocument()", 200);
