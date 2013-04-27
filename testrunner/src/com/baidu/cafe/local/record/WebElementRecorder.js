29132a8c51887787679f8ad4f60e9135483fb1ec
leiming <leiming@leiming-HP-Compaq-6000-Pro-MT-PC.(none)>
2013-04-27 14:21:28 +0800

    save

--- testrunner/src/com/baidu/cafe/local/record/WebElementRecorder.js
+++ testrunner/src/com/baidu/cafe/local/record/WebElementRecorder.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var Cafe = function() {};
 Cafe.prototype.getElementFamilyString = function(element) {
     var familyString = '';
@@ -64,42 +63,4 @@ Cafe.prototype.hookWebDocument = function() {
 }
 var _cafe = new Cafe();
 setInterval("_cafe.hookWebDocument()", 200);
-=======
-if(typeof _cafe === "undefined") {
-	function getElementFamilyString(element) {
-	    var familyString = '';
-	    for ( ; element && element.nodeType == 1; element = element.parentNode ) {
-	        var children = element.parentNode.childNodes;
-	        var id = '';
-	        var i = 0;
-	        for(; i < children.length; i++) {
-	            if (children[i] === element) {
-	                break;
-	            }
-	        }
-	        familyString = '-' + i + familyString;
-	    }
-	    return familyString.substring(1);
-	}
-	function onClickCallback() {
-		var event = arguments[0];
-		prompt(event.type + ";,local.clickOnWebElementByFamilyString(\"" + getElementFamilyString(event.target) + "\");");
-		finished();
-	}
-	function onChangeCallback() {
-		var event = arguments[0];
-		prompt(event.type + ";,local.enterTextInWebElementByFamilyString(\"" + getElementFamilyString(event.target) + "\"), \"" + event.target.value + "\");");
-		finished();
-	}
-	function finished() {
-		prompt('WebElementRecorder-finished');
-	}
-	
-	document.removeEventListener('click', onClickCallback);
-	document.removeEventListener('change', onChangeCallback);
-	
-	document.addEventListener('click', onClickCallback, false);
-	document.addEventListener('change', onChangeCallback, false);
-	var _cafe = new Object();
-}
->>>>>>> 69518b12e6d8d78b2fd5cc8b4c83503ba97b2743
+
